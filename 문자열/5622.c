c9cfe871a87a793b26dcbd79905a34d029f596d6
Kim Dohyun <kimdohyun.x@gmail.com>
2025-01-01 12:13:53 +0900

    5622 및 더 간결한 코드

--- 문자열/5622.c
+++ 문자열/5622.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
 
 int main() {
+    char s[16], alpha[27]={0};
+    scanf("%s", s);
     
+    for(int i=1, j=0; j<15; i++, j+=3){
+        alpha[j]=2+i;
+        alpha[j+1]=2+i;
+        alpha[j+2]=2+i;
+    }
+
+    alpha[15]=8;
+    alpha[16]=8;
+    alpha[17]=8;
+    alpha[18]=8;
+
+    alpha[19]=9;
+    alpha[20]=9;
+    alpha[21]=9;
+    
+    alpha[22]=10;
+    alpha[23]=10;
+    alpha[24]=10;
+    alpha[25]=10;
+
+
+    // 지금부터 문자열 알파벳 변환 작업~
+    int sum=0;
+
+    for(int i=0; s[i]!=0; i++){
+        s[i]=s[i]-'A';      // 알파벳을 숫자로 변환
+        s[i]=alpha[s[i]];   //
+        sum+=s[i];
+    }
+
+    printf("%d", sum);
 
     return 0;
 }
---
+++ 문자열/5622plus.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char s[16];
+    scanf("%s", s);
+    
+    int alpha[26]={3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,8,9,9,9,10,10,10,10};
+
+
+    // 지금부터 문자열 알파벳 변환 작업~
+    int sum=0;
+
+    for(int i=0; s[i]!=0; i++){
+        s[i]=s[i]-'A';      // 알파벳을 숫자로 변환
+        s[i]=alpha[s[i]];   //
+        sum+=s[i];
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
