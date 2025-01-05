75293fc59b20bb6e88e4b0d99ce032923e61f26c
Kim Dohyun <kimdohyun.x@gmail.com>
2025-01-05 12:47:52 +0900

    심화 파일 업로드

---
+++ 심화/25083.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    printf("         ,r'\"7\n");
+    printf("r`-_   ,'  ,/\n");
+    printf(" \\. \". L_r\'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |\n");
+
+    return 0;
+}
+
+// 큰 따옴표 앞에만 \(역슬래시)를 붙여주면 된다.
